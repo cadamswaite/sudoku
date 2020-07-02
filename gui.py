dbe0b5ed0669f3ae1100c3b1496834a5336565d4
cadamswaite <cadamswaite@users.noreply.github.com>
2020-07-02 23:15:27 +0100

    Allow basic user interaction with the gui.

--- gui.py
+++ gui.py
@@ -15,6 +15,11 @@ class Square:
         self.grid = grid
         self.possibles = {x+1 for x in range(9)}
         self.value = 0
+        self.selected = False
+        self.rect = pygame.Rect(self.center_pos(self.x) - self.grid.scale/2,
+                                self.center_pos(self.y) - self.grid.scale/2,
+                                self.grid.scale,
+                                self.grid.scale)
 
     def center_pos(self, x_or_y):
         """Given the x or y position of this square, returns the coordinates"""
@@ -24,11 +29,12 @@ class Square:
 
     def erase(self):
         """ Erases the current square in the grid """
-        pygame.draw.rect(self.grid.screen, self.grid.col_bg,
-                         (self.center_pos(self.x) - self.grid.scale/2,
-                          self.center_pos(self.y) - self.grid.scale/2,
-                          self.grid.scale,
-                          self.grid.scale), 0)
+        if self.selected:
+            col = self.grid.sel_bg
+        else:
+            col = self.grid.col_bg
+        pygame.draw.rect(self.grid.screen, col, self.rect, 0)
+
     def draw_pos(self):
         """ Add text for all possibles """
         for pos in self.possibles:
@@ -66,12 +72,14 @@ class Grid:
         for square in self.squaredict.values():
             square.erase()
             square.draw()
+        pygame.display.update()
 
     def __init__(self):
         self.scale = 70
         self.grid_width = 2
         self.grid_break = 5
         self.col_bg = (255, 255, 255)
+        self.sel_bg = (200, 200, 255)
 
         pygame.font.init()
         self.poss_font = pygame.font.SysFont('Comic Sans MS', 30)
@@ -83,6 +91,36 @@ class Grid:
         self.screen = pygame.display.set_mode((9*(self.scale+self.grid_width)+2*self.grid_break,
                                                9*(self.scale+self.grid_width)+2*self.grid_break))
 
+    def clicked_square(self, point):
+        """ Returns the clicked square when given x,y within the rectangle """
+        for square in self.squaredict.values():
+            if square.rect.collidepoint(point):
+                return square
+        return None
+
+    def deselect_squares(self):
+        """ Deselects all squares, usually before selecting a new one"""
+        for square in self.squaredict.values():
+            square.selected = False
+
+def eventloop(grid):
+    """ Main eventloop for detecting user interaction with pygame"""
+    running = True
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                square = grid.clicked_square(pos)
+                if square is None:
+                    break
+                grid.deselect_squares()
+                square.selected = True
+                grid.show_grid()
+
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 def main():
     """
     Main function
@@ -90,8 +128,7 @@ def main():
     grid = Grid()
     grid.squaredict[(3, 4)].value = 5
     grid.show_grid()
-    pygame.display.update()
-    input()
+    eventloop(grid)
 
 if __name__ == '__main__':
     main()
